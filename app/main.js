44789e581b551db0636d0198c008c8f9104c4633
LUk3VIT <lucasdiaa061@gmail.com>
2025-03-31 16:50:09 -0300

    0.3.11

--- app/main.js
+++ app/main.js
@@ -1,1 +1,18 @@
+const som = document.querySelector("#addVida");
+const sub = document.querySelector("#subVida");
+const vida = document.querySelector("#vida");
+
+som.addEventListener("click", (evento) => {
+    vida.value = parseInt(vida.value) + 1;
+});
+
+sub.addEventListener("click", (evento) => {
+    vida.value = parseInt(vida.value) - 1;
+});
+
+const calcAtrib = document.querySelector("#calcAtrib");
+const forc = document.getElementById("#for");
+const Rforc = document.querySelector("#Rfor");
+
+
 
