b1e7211d902bef2608c9d266c2004db0dab7ba36
LUk3VIT <lucasdiaa061@gmail.com>
2025-04-01 15:45:37 -0300

    0.4.11

--- app/main.js
+++ app/main.js
@@ -10,9 +10,31 @@ sub.addEventListener("click", (evento) => {
     vida.value = parseInt(vida.value) - 1;
 });
 
-const calcAtrib = document.querySelector("#calcAtrib");
-const forc = document.getElementById("#for");
-const Rforc = document.querySelector("#Rfor");
+const calcAtrib = document.querySelector("calcAtrib");
+const Rforc = document.querySelector("Rfor");
+
+function calcAtribOnClick(){
+    var forc = document.getElementById("for").value;
+    var des = document.getElementById("des").value;
+    var con = document.getElementById("con").value;
+    var int = document.getElementById("int").value;
+    var sab = document.getElementById("sab").value;
+    var car = document.getElementById("car").value;
+
+    var atrib = [forc, des, con, int, sab, car];
+    
+
+    for (let i = 0; i < 5; i++){
+        atrib[i];
+
+        for (let l = 0; l < 1; l++){
+            if (atrib[i] == 1){
+                
+            }
+        }
+    }
+    
+}
 
 
 
