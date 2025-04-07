98b4578c51ebe91378cb8a2c7cd33d2c445ec0df
LUk3VIT <lucasdiaa061@gmail.com>
2025-04-07 11:59:24 -0300

    0.5.11

--- app/main.js
+++ app/main.js
@@ -11,30 +11,72 @@ sub.addEventListener("click", (evento) => {
 });
 
 const calcAtrib = document.querySelector("calcAtrib");
-const Rforc = document.querySelector("Rfor");
 
 function calcAtribOnClick(){
-    var forc = document.getElementById("for").value;
-    var des = document.getElementById("des").value;
-    var con = document.getElementById("con").value;
-    var int = document.getElementById("int").value;
-    var sab = document.getElementById("sab").value;
-    var car = document.getElementById("car").value;
+    const forc = document.getElementById("for").value;
+    const des = document.getElementById("des").value;
+    const con = document.getElementById("con").value;
+    const int = document.getElementById("int").value;
+    const sab = document.getElementById("sab").value;
+    const car = document.getElementById("car").value;
 
-    var atrib = [forc, des, con, int, sab, car];
-    
+    let Rforc = document.getElementById("Rfor");
+    let Rdes = document.getElementById("Rdes");
+    let Rcon = document.getElementById("Rcon");
+    let Rint = document.getElementById("Rint");
+    let Rsab = document.getElementById("Rsab");
+    let Rcar = document.getElementById("Rcar");
 
-    for (let i = 0; i < 5; i++){
+    const atrib = [forc, des, con, int, sab, car];
+    const Ratrib = [Rforc, Rdes, Rcon, Rint, Rsab, Rcar]
+
+    for (let i = 0; i < 6; i++){
         atrib[i];
+        Ratrib[i]
 
         for (let l = 0; l < 1; l++){
             if (atrib[i] == 1){
-                
+               Ratrib[i].innerHTML = "-5";
+            } else if (atrib[i] == 2 || atrib[i] == 3){
+                Ratrib[i].innerHTML = "-4"
+            } else if (atrib[i] == 4 || atrib[i] == 5){
+                Ratrib[i].innerHTML = "-3"
+            } else if (atrib[i] == 6 || atrib[i] == 7){
+                Ratrib[i].innerHTML = "-2"
+            } else if (atrib[i] == 8 || atrib[i] == 9){
+                Ratrib[i].innerHTML = "-1"
+            } else if (atrib[i] == 10 || atrib[i] == 11){
+                Ratrib[i].innerHTML = "0"
+            } else if (atrib[i] == 12 || atrib[i] == 13){
+                Ratrib[i].innerHTML = "+1"
+            } else if (atrib[i] == 14 || atrib[i] == 15){
+                Ratrib[i].innerHTML = "+2"
+            } else if (atrib[i] == 16 || atrib[i] == 17){
+                Ratrib[i].innerHTML = "+3"
+            } else if (atrib[i] == 18 || atrib[i] == 19){
+                Ratrib[i].innerHTML = "+4"
+            } else if (atrib[i] == 20 || atrib[i] == 21){
+                Ratrib[i].innerHTML = "+5"
+            } else if (atrib[i] == 22 || atrib[i] == 23){
+                Ratrib[i].innerHTML = "+6"
+            } else if (atrib[i] == 24 || atrib[i] == 25){
+                Ratrib[i].innerHTML = "+7"
+            } else if (atrib[i] == 26 || atrib[i] == 27){
+                Ratrib[i].innerHTML = "+8"
+            } else if (atrib[i] == 28 || atrib[i] == 29){
+                Ratrib[i].innerHTML = "+9"
+            } else if (atrib[i] == 30){
+                Ratrib[i].innerHTML = "+10"
+            } else {
+                Ratrib[i].innerHTML = "0"
             }
         }
     }
+
+    console.log(Ratrib)
     
 }
+
 
 
 
